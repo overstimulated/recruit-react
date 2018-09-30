7aa718939071412c10978af85ce1c5050779901d
JmeSolomon <jme2go@yahoo.com>
2018-10-01 03:37:25 +1300

    created store with reduxThunk middleware

---
+++ src/store.ts
@@ -0,0 +1,6 @@
+import { applyMiddleware, compose, createStore, Store } from 'redux';
+import reduxThunk from 'redux-thunk';
+import { IState, state } from './reducers';
+
+export const store: Store<IState> = createStore(state,
+    compose(applyMiddleware(reduxThunk)));
